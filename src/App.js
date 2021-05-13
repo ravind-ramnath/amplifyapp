487bb068f305d78f9aa4baa74ea10866f54e7a39
Ravind Ramnath <ravind@TVNZ2020236.local>
2021-05-13 15:55:27 +1200

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TVNZ EPG
+          TVNZ EPG v1
         </p>       
       </header>
     </div>
